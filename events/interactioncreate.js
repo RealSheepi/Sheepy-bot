e05390d9fc179b9d8290269b6e61f700c2b8e404
RealSheepi <realsheep1234@gmail.com>
2024-07-16 13:59:14 +0530

    Fixed the interactionCreate event!

--- events/interactioncreate.js
+++ events/interactioncreate.js
@@ -1,17 +1,49 @@
-module.exports = {
-    name: 'interactionCreate',
-    async execute(interaction, client) {
-        if (!interaction.isCommand()) return;
+const { EmbedBuilder, Collection, InteractionType } = require('discord.js');
+const cooldowns = new Collection();
 
-        const command = client.commands.get(interaction.commandName);
+module.exports = async interaction => {
+    const { client } = interaction;
+    if (interaction.type !== InteractionType.ApplicationCommand) return;
 
-        if (!command) return;
+    const command = client.commands.get(interaction.commandName);
 
-        try {
-            await command.execute(interaction, client);
-        } catch (error) {
-            console.error(error);
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    if (!command) return;
+
+    if (command.guildOnly && interaction.channel.type === 1) {
+        return interaction.reply({ embeds: [global.errors[0]] });
+    }
+
+    if (!cooldowns.has(command.data.name)) {
+        cooldowns.set(command.data.name, new Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.data.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+
+    if (timestamps.has(interaction.user.id)) {
+        const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+
+            const inCooldown = new EmbedBuilder()
+                .setTitle('Cooldown')
+                .setDescription(`Please wait \`${timeLeft.toFixed(1)}\` more second(s) before reusing the **${command.data.name}** command.`)
+                .setColor('#ffaa00');
+            return interaction.reply({ embeds: [inCooldown], ephemeral: true });
         }
-    },
+    }
+
+    timestamps.set(interaction.user.id, now);
+    setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
+    try {
+        const configuration = require('../config.json');
+        await command.execute(interaction, configuration);
+    }
+    catch (error) {
+        console.error(error);
+        await interaction.reply({ embeds: [global.errors[4]] });
+    }
 };
